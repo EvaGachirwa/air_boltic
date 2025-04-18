a1b369dd7449c42e93da4adccc95011d2f9c176a
Eva Mwangi <eva.mwangi@pleo.io>
2025-04-18 13:13:28 +0200

    set up extract

--- elt/extract.py
+++ elt/extract.py
@@ -1,5 +1,26 @@
 import json
+import pandas as pd
+from sqlalchemy import create_engine
 
-with open('/data/aeroplane_model.json') as f:
+with open('../data/aeroplane_model.json') as f:
     d = json.load(f)
     print(d)
+
+fleet = []
+
+for item in d:
+    aeroplane = {}
+    print(item)
+    aeroplane['type'] = item
+    for indi in d[item]:
+        aeroplane['series'] = indi
+        aeroplane['max_seats'] = d[item][indi]['max_seats']
+        aeroplane['max_weight'] = d[item][indi]['max_weight']
+        aeroplane['max_distance'] = d[item][indi]['max_distance']
+        aeroplane['engine_type'] = d[item][indi]['engine_type']
+    fleet.append(aeroplane)
+
+df = pd.DataFrame(fleet)
+
+engine = create_engine('sqlite://', echo=False)
+df.to_sql(name='aeroplane_model', con=engine, if_exists='append')
