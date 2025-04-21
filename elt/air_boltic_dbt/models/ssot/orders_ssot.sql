38633435f9bba9e21934f52b18ad5f3ff8dc8f1b
Eva Mwangi <eva.mwangi@pleo.io>
2025-04-21 14:35:40 +0200

    refine ssots to remove errors

--- elt/air_boltic_dbt/models/ssot/orders_ssot.sql
+++ elt/air_boltic_dbt/models/ssot/orders_ssot.sql
@@ -5,10 +5,10 @@ with staged_data as (
     select * from {{ ref('orders_staging') }} orders
     LEFT JOIN {{ ref('customers_staging') }} as customer
     ON
-        orders.'customer ID' = customer.ID
+        orders.'customer ID' = customer.'Customer ID'
     LEFT JOIN {{ ref('trips_staging') }} trips
     ON
-        orders.'Trip ID' = trips.ID
+        orders.'Trip ID' = trips.'Trip ID'
 
 )
 
